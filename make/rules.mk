5daa2733ca813d3732ad93e87584f74a1792ed16
simplex <theemptystring@gmail.com>
2013-09-01 23:07:34 -0300

    tweaked rules for subtree pulling

--- make/rules.mk
+++ make/rules.mk
@@ -50,8 +50,8 @@ $(info Setting up targets...)
 dist: $(PROJECT_ZIP) $(WICKER_GENERATED_FILES)
 
 wicker:
-	-git subtree pull --prefix $(SCRIPT_DIR)/wicker https://github.com/nsimplex/wicker.git --squash
-	-git subtree pull --prefix $(TOOLS_DIR) https://github.com/nsimplex/wickertools.git --squash
+	-git subtree pull --prefix $(SCRIPT_DIR)/wicker https://github.com/nsimplex/wicker.git master --squash
+	-git subtree pull --prefix $(TOOLS_DIR) https://github.com/nsimplex/wickertools.git master --squash
 
 boot: $(TOOLS_DIR)/bootup_gen.pl
 	find "$(SCRIPT_DIR)" -type f -name '*.lua' -exec perl "$<" '{}' \;
