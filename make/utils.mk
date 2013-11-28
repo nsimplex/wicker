68dc9941f714f7b992b96b2499f22837bff8868c
simplex <theemptystring@gmail.com>
2013-11-28 23:02:43 +0000

    Tweak in utils.mk

--- make/utils.mk
+++ make/utils.mk
@@ -5,9 +5,12 @@ ifndef WICKER_TOOLS_DIR
  $(error WICKER_TOOLS_DIR is not defined)
 endif
 
+ifndef WICKER_SCRIPT_DIR
+ WICKER_SCRIPT_DIR:=scripts/wicker
+endif
 
 wicker:
-	-git subtree pull --prefix $(SCRIPT_DIR)/wicker https://github.com/nsimplex/wicker.git master --squash
+	-git subtree pull --prefix $(WICKER_SCRIPT_DIR) https://github.com/nsimplex/wicker.git master --squash
 	-git subtree pull --prefix $(WICKER_TOOLS_DIR) https://github.com/nsimplex/wickertools.git master --squash
 
 boot: $(WICKER_TOOLS_DIR)/bootup_gen.pl
