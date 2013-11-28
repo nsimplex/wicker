1d3e06fe32f060906502da028fe8d01a16a07ab6
simplex <theemptystring@gmail.com>
2013-11-28 22:49:56 +0000

    utils.mk tweak

--- make/utils.mk
+++ make/utils.mk
@@ -11,4 +11,4 @@ wicker:
 	-git subtree pull --prefix $(WICKER_TOOLS_DIR) https://github.com/nsimplex/wickertools.git master --squash
 
 boot: $(WICKER_TOOLS_DIR)/bootup_gen.pl
-	find "$(SCRIPT_DIR)" -type f -name '*.lua' -exec perl "$<" '{}' global \;
+	find "$(SCRIPT_DIR)" -type f -name '*.lua' -exec perl "$<" '{}' mod \;
