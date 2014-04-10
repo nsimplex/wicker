3ee21c1eb54c5ad41b86ce580d66d423608316a5
simplex <theemptystring@gmail.com>
2014-04-10 15:10:14 +0100

    AddPlayerPrefabPostInit

---
+++ plugins/addplayerprefabpostinit.lua
@@ -0,0 +1,24 @@
+local FunctionQueue = wickerrequire "gadgets.functionqueue"
+
+local postinits = FunctionQueue()
+
+TheMod:AddPrefabPostInit("world", function()
+	assert( _G.GetPlayer() == nil )
+	local player_prefab = _G.SaveGameIndex:GetSlotCharacter()
+ 
+	-- Unfortunately, we can't add new postinits by now. So we have to do
+	-- it the hard way...
+ 
+	_G.TheSim:LoadPrefabs( {player_prefab} )
+	local oldfn = _G.Prefabs[player_prefab].fn
+	_G.Prefabs[player_prefab].fn = function(Sim)
+		local inst = oldfn(Sim)
+		postinits(inst)
+		return inst
+	end
+end)
+
+local function AddPlayerPrefabPostInit(fn)
+	table.insert(postinits, fn)
+end
+TheMod:EmbedHook("AddPlayerPrefabPostInit", AddPlayerPrefabPostInit)
